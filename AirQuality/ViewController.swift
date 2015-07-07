55989626ee01ac76c095c61502b69ba75f43877e
Xu <lvxu@lm-shd-00950796.paypalcorp.com>
2015-07-07 14:06:09 +0800

    first commit

---
+++ AirQuality/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AirQuality
+//
+//  Created by Xu, Lvcai on 6/11/15.
+//  Copyright © 2015 Xu, Lvcai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
